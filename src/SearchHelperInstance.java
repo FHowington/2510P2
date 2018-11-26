bcd6d69f62099a0d69d7662c16cd7ce360f67ca2
Forbes Turley <fot3@pitt.edu>
2018-11-26 07:09:19 -0500

    Began building search

---
+++ src/SearchHelperInstance.java
@@ -0,0 +1,54 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SearchHelperInstance extends Server {
+
+    private static final int SERVER_PORT = 8765;
+
+    private SearchHelperInstance() {
+        super(SERVER_PORT, "ALPHA");
+    }
+
+    private SearchHelperInstance(int _port) {
+        super(_port, "ALPHA");
+    }
+
+
+
+    // Basically we should not send out more reqs when we are still waiting to hear back on current batch
+    public void start() {
+        try {
+
+            final ServerSocket serverSock = new ServerSocket(port);
+
+            Socket sock;
+            SearchHelperThread thread ;
+
+
+            while (true) {
+                sock = serverSock.accept();
+                thread = new SearchHelperThread(sock, this);
+                thread.start();
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            try {
+                SearchHelperInstance server = new SearchHelperInstance(Integer.parseInt(args[0]));
+                server.start();
+            } catch (NumberFormatException e) {
+                System.out.printf("Enter a valid port number\n",
+                        SearchHelperInstance.SERVER_PORT);
+            }
+        } else {
+            SearchHelperInstance server = new SearchHelperInstance();
+            server.start();
+        }
+    }
+}
+
---
+++ src/SearchHelperThread.java
@@ -0,0 +1,44 @@
+import java.lang.Thread;
+import java.net.Socket;
+import java.io.*;
+import java.util.HashMap;
+
+public class SearchHelperThread extends Thread {
+    private final Socket socket;
+    private SearchHelperInstance gs;
+
+    private HashMap<String, Integer> result;
+
+    SearchHelperThread(Socket _socket, SearchHelperInstance _gs) {
+        socket = _socket;
+        this.gs = _gs;
+        result = new HashMap<>();
+    }
+
+    public void run() {
+        boolean proceed = true;
+
+        try {
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+
+            while (proceed) {
+                Envelope message = (Envelope) input.readObject();
+                System.out.println("Received message: " + message.getMessage());
+                if (message.getMessage().equals("INDEX")) {
+                    System.out.println("Got index request");
+                    
+                    Envelope response = new Envelope("TOKENS");
+                    response.addObject(result);
+                    output.writeObject(response);
+                    return;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace(System.err);
+        }
+    }
+
+
+}
