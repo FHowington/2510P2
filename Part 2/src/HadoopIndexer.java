44bb1af8f0289eac9eb3d59448053523c94369f5
Jonathan Albert <JMA85@pitt.edu>
2018-12-01 18:40:33 -0500

    Moved DocumentWordPair to the file where Writable interfaces will be implemented

--- Part 2/src/HadoopIndexer.java
+++ Part 2/src/HadoopIndexer.java
@@ -125,28 +125,3 @@ public class HadoopIndexer
         }
     }
 }
-
-/**
- * This class is a simple container to hold a term's "posting"
- * (using the verbiage of the given PowerPoint slides)
- */
-class DocumentWordPair
-{
-    public DocumentWordPair(String path, Text word, LongWritable count)
-    {
-        this.filePath = path;
-        this.word = word;
-        this.count = count;
-    }
-
-    /** The document ID */
-    public String filePath;
-    /**
-     * The term in the document being tracked by this pair.
-     * This is likely unnecessary, considering how the mappers
-     * and reducers keep track of the word as the key already
-     */
-    public Text word;
-    /** The number of occurrences of the word in the document*/
-    public LongWritable count;
-}
--- Part 2/src/IndexEntry.java
+++ Part 2/src/IndexEntry.java
@@ -1,4 +1,6 @@
 import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 
 public class IndexEntry extends ArrayWritable
 {
@@ -6,4 +8,29 @@ public class IndexEntry extends ArrayWritable
     {
         super(IndexEntry.class);
     }
+}
+
+/**
+ * This class is a simple container to hold a term's "posting"
+ * (using the verbiage of the given PowerPoint slides)
+ */
+class DocumentWordPair
+{
+    public DocumentWordPair(String path, Text word, LongWritable count)
+    {
+        this.filePath = path;
+        this.word = word;
+        this.count = count;
+    }
+
+    /** The document ID */
+    public String filePath;
+    /**
+     * The term in the document being tracked by this pair.
+     * This is likely unnecessary, considering how the mappers
+     * and reducers keep track of the word as the key already
+     */
+    public Text word;
+    /** The number of occurrences of the word in the document*/
+    public LongWritable count;
 }
